f4d8e0718df2f78e99eff2637839550073a56f36
Gulay <gulaynasibova28@gmail.com>
2024-12-27 13:55:57 +0400

    js-ders15-task1

---
+++ app.js
@@ -0,0 +1,8 @@
+let arr = [
+    ['alma','qirmizi'],
+    ['alça','yaşıl'],
+    ['gilas','qirmizi'],
+    ['heyva','sarı'],
+    ]
+let furit = arr.filter(element => element[1] == "qirmizi")
+console.log(furit)
